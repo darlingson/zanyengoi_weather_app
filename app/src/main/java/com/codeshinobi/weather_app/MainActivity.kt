36014a5f4d7e4e9c16b85c8422077edd5d36e841
darlingson <dmakuwila@gmail.com>
2024-02-14 13:21:37 +0200

    added a picture of blantyre as background image

--- app/src/main/java/com/codeshinobi/weather_app/MainActivity.kt
+++ app/src/main/java/com/codeshinobi/weather_app/MainActivity.kt
@@ -3,12 +3,16 @@ package com.codeshinobi.weather_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.codeshinobi.weather_app.ui.theme.Weather_appTheme
 
@@ -22,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    HomeScreenMain()
                 }
             }
         }
@@ -31,7 +36,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreenMain() {
+    Box(
+        modifier = with (Modifier){
+            fillMaxSize()
+                .paint(
+                    // Replace with your image id
+                    painterResource(id = R.drawable.blantyre_large),
+                    contentScale = ContentScale.FillBounds)
 
+        })
+    {
+        Greeting(name = "Darlingson")
+    }
 }
 
 @Composable
