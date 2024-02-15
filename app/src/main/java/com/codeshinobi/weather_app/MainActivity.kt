e942823f173f25b305f6dd92447f33ba9451fcb4
darlingson <dmakuwila@gmail.com>
2024-02-15 21:59:38 +0200

    added api response array shape for reference

--- app/src/main/java/com/codeshinobi/weather_app/MainActivity.kt
+++ app/src/main/java/com/codeshinobi/weather_app/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,8 +65,34 @@ fun HomeScreenMain() {
     {
         Column {
             Greeting(name = "Darlingson")
-            CurrentTempCard()
+            Row(Modifier.fillMaxWidth()) {
+                CurrentTempCard()
+                Spacer(Modifier.weight(1f))
+                TimeIndicatorIcon()
+            }
         }
+
+    }
+}
+@Composable
+fun TimeIndicatorIcon() {
+    val sdf = SimpleDateFormat("HH")
+    val currentTime = sdf.format(System.currentTimeMillis())
+    var greetingText = painterResource(id = R.drawable.morning)
+    if (currentTime.toInt() < 12) {
+        greetingText = painterResource(id = R.drawable.morning)
+    }
+    else if (currentTime.toInt() < 16) {
+        greetingText = painterResource(id = R.drawable.sun_one)
+    }
+    else if (currentTime.toInt() < 28) {
+        greetingText = painterResource(id = R.drawable.sunset)
+    }
+    else {
+        greetingText = painterResource(id = R.drawable.moon_4214952)
+    }
+    Card {
+        Image(painter = greetingText, contentDescription = "Current time Icon")
     }
 }
 @OptIn(ExperimentalMaterial3Api::class)
@@ -72,25 +105,40 @@ fun CurrentTempCard(){
     val dow = SimpleDateFormat("EEEE")
     val d = Date()
     val dayOfTheWeek = dow.format(d)
+    val fontSizeLarge = MaterialTheme.typography.titleLarge.fontSize
+    val fontSizeMedium = MaterialTheme.typography.titleMedium.fontSize
     Card(
         onClick = { /*TODO*/ },
-        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
         colors = CardDefaults.cardColors(
             containerColor = Color.Transparent
-        )
+        ),
+        modifier = Modifier.padding(10.dp)
         ) {
         Column(Modifier.padding(10.dp)) {
+            showImage()
             CurrentTemp(name = "Darlingson")
-            Text(text = currentDateAndTime)
-            Text(text = dayOfTheWeek)
+            Text(text = currentDateAndTime, fontSize = fontSizeMedium)
+            Text(text = dayOfTheWeek, fontSize = fontSizeMedium)
         }
     }
 }
 @Composable
 fun CurrentTemp(name: String) {
+
     Box() {
-        Text(text = "22 °C", modifier = Modifier.padding(10.dp))
+        Text(text = "22 °C", style = MaterialTheme.typography.titleLarge)
     }
+}
+@Composable
+fun showImage(){
+    Image(
+        painter = painterResource(id = R.drawable.sun),
+        contentDescription = "Icon for the temperature",
+        modifier = Modifier
+            .padding(10.dp)
+            .height(100.dp)
+    )
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
