cc3d1616f1353c8bdf18bf7cc91572bd98962918
darlingson <dmakuwila@gmail.com>
2024-02-16 19:21:19 +0200

    added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
